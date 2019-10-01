1757bac90c316e63dc793cb1cc050c11ac0dbabf
Gavin Uberti <guberti@eastsideprep.org>
2019-09-30 19:35:39 -0700

    Add error introduction capacity to VMH

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/mecanum/VirtualMecanumHardware.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/mecanum/VirtualMecanumHardware.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.robot.mecanum;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.simulator.VirtualRobot;
 import org.firstinspires.ftc.teamcode.autonomous.odometry.StandardTrackingWheelLocalizer;
@@ -16,22 +17,17 @@ import org.mockito.Mockito;
 import org.openftc.revextensions2.RevBulkData;
 
 public class VirtualMecanumHardware extends MecanumHardware implements VirtualRobot {
-    double TRACK_WIDTH = 17;
+    public static double TRACK_WIDTH = 17;
 
-    double MAX_FORWARD_SPEED = 60; // Per second
-    double MAX_STRAFE_SPEED = 50;
+    public static double MAX_FORWARD_SPEED = 60; // Per second
+    public static double MAX_STRAFE_SPEED = 50;
 
-    double DECAY_FRAC = 0.1;
+    public static double DECAY_FRAC = 0.1; // How fast speed decays off the robotbj
+    public static Pose MAX_ACCERATIONS = new Pose(MAX_FORWARD_SPEED, MAX_STRAFE_SPEED,
+            MAX_FORWARD_SPEED/TRACK_WIDTH);
 
-    Pose TERMINAL_VELOCITIES = new Pose(MAX_FORWARD_SPEED, MAX_STRAFE_SPEED, MAX_FORWARD_SPEED/TRACK_WIDTH);
-    // TODO replace these with better, measured values
-    Pose MAX_ACCERATIONS = new Pose(MAX_FORWARD_SPEED, MAX_STRAFE_SPEED, MAX_FORWARD_SPEED/TRACK_WIDTH);
-
-    // At terminal velocity, the robot's frictional acceleration will be equal to the max acceleration,
-    // but opposite in direction
-    Pose STATIC_FRICTION = new Pose(20, 20, 1);
-    Pose KINETIC_FRICTION = TERMINAL_VELOCITIES.clone().scale(-1);
-
+    public static double MAX_VOLTAGE_ERROR = 0.15; // 1.8 V
+    public static double POWER_NONLINEARITY = 1;
 
     Pose position;
     Pose velocity;
@@ -86,44 +82,42 @@ public class VirtualMecanumHardware extends MecanumHardware implements VirtualRo
             throw new AssertionError();
         }
 
+        MecanumPowers errPowers = new MecanumPowers(
+                addVoltageError(wheelPowers.frontLeft),
+                addVoltageError(wheelPowers.frontRight),
+                addVoltageError(wheelPowers.backLeft),
+                addVoltageError(wheelPowers.backRight)
+        );
+
         // +y is to the left
         // Calculations are simple because our wheels are oriented at 45 degrees
         Pose acceleration = new Pose(
-                (wheelPowers.frontLeft +
-                    wheelPowers.frontRight +
-                    wheelPowers.backLeft +
-                    wheelPowers.backRight) / 4,
+                (errPowers.frontLeft +
+                    errPowers.frontRight +
+                    errPowers.backLeft +
+                    errPowers.backRight) / 4,
 
-                (wheelPowers.frontRight +
-                    wheelPowers.backLeft +
-                    -wheelPowers.frontLeft +
-                    -wheelPowers.backRight) / 4,
+                (errPowers.frontRight +
+                    errPowers.backLeft +
+                    -errPowers.frontLeft +
+                    -errPowers.backRight) / 4,
 
-            (wheelPowers.frontRight +
-                    wheelPowers.backRight +
-                    -wheelPowers.frontLeft +
-                    -wheelPowers.backLeft) / 4
+            (errPowers.frontRight +
+                     errPowers.backRight +
+                     -errPowers.frontLeft +
+                     -errPowers.backLeft) / 4
         ).multiply(MAX_ACCERATIONS);
 
-        if (false) {
-            acceleration.applyFriction(STATIC_FRICTION);
+        velocity = velocity.scale(1 - DECAY_FRAC).add(acceleration.scale(DECAY_FRAC));
+        position = MathUtil.relativeOdometryUpdate(position, velocity.scale(secs));
 
-            Pose velocityDelta = acceleration.scale(secs);
-            velocity = velocity.add(velocityDelta);
-            velocity.clampAbs(TERMINAL_VELOCITIES);
-            System.out.println(velocity);
-            System.out.println(velocityDelta);
-            System.out.println(secs);
-
-            Pose positionDelta = velocity.scale(secs);
-            position = MathUtil.relativeOdometryUpdate(position, positionDelta);
-        } else if (true) {
-            velocity = velocity.scale(1 - DECAY_FRAC).add(acceleration.scale(DECAY_FRAC));
-            position = MathUtil.relativeOdometryUpdate(position, velocity.scale(secs));
-        } else {
-            position = MathUtil.relativeOdometryUpdate(position, acceleration.scale(secs));
-        }
         time += secs;
+    }
+
+    public static double addVoltageError(double d) {
+        double randomFrac = 0;//(Math.random() * 2) - 1; // Between -1 and 1
+        double nonlinear = Math.copySign(Math.pow(Math.abs(d), POWER_NONLINEARITY), d);
+        return Range.clip(nonlinear + randomFrac, -1, 1);
     }
 
     @Override
