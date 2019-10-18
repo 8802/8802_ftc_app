179dc514976f4f5811140ad3f3b478d2bdbf0443
Gavin Uberti <guberti@eastsideprep.org>
2019-10-18 14:39:27 -0700

    Better mecanum PP controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/controllers/MecanumPurePursuitController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/controllers/MecanumPurePursuitController.java
@@ -1,24 +1,28 @@
 package org.firstinspires.ftc.teamcode.autonomous.controllers;
 
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.HeadingControlledWaypoint;
+import org.firstinspires.ftc.teamcode.autonomous.waypoints.StopWaypoint;
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.Waypoint;
 import org.firstinspires.ftc.teamcode.common.math.MathUtil;
+import org.firstinspires.ftc.teamcode.common.math.Point;
 import org.firstinspires.ftc.teamcode.common.math.Pose;
 import org.firstinspires.ftc.teamcode.robot.mecanum.MecanumPowers;
 
 public class MecanumPurePursuitController {
-    public static double REDUCE_TURN_SPEED_THRESHOLD = Math.PI/2;
-    public static double REDUCE_MOVE_SPEED_THRESHOLD = 30;
+    // How far we slip if we're moving with max power in each of these directions
+    public static Pose SLIP_DISTANCES = new Pose(22, 5, Math.toRadians(30));
+    public static double MIN_TRANSLATION_POWERS = 0.2;
+    public static double CUTOFF_MOTOR_POWER = 0.05;
+    public static Pose GUNNING_REDUCTION_DISTANCES = new Pose(6, 6, Math.PI/2);
+    public static Pose FINE_REDUCTION_DISTANCES = new Pose(30, 30, Math.PI);
 
-    public static double GUNNING_REDUCE_TURN_SPEED_THRESHOLD = Math.PI/8;
-    public static double GUNNING_REDUCE_MOVE_SPEED_THRESHOLD = 6;
 
     public static MecanumPowers goToPosition(Pose robotPose, Waypoint target, double movementSpeed, boolean gunning) {
         //System.out.println(target);
         //System.out.println(robotPose);
 
-        double reduceMoveSpeedThreshold = gunning ? GUNNING_REDUCE_MOVE_SPEED_THRESHOLD : REDUCE_MOVE_SPEED_THRESHOLD;
-        double reduceTurnSpeedThreshold = gunning ? GUNNING_REDUCE_TURN_SPEED_THRESHOLD : REDUCE_TURN_SPEED_THRESHOLD;
+        // Sometimes we need to move accurately to a position, while many other times we just have
+        // to get "about" somewhere - our waypoints are approximations anyway. If we need to be exact
 
         double distance = target.minus(robotPose).radius();
         double relAngle = robotPose.minus(target).atan() - robotPose.heading;
@@ -26,23 +30,32 @@ public class MecanumPurePursuitController {
         double relY = distance * Math.sin(relAngle);
 
         // We negate x and y power because we want to move in the opposite direction of our error
-        double xPower = movementSpeed * -relX / reduceMoveSpeedThreshold;
-        double yPower = movementSpeed * -relY / reduceMoveSpeedThreshold;
+        Pose translationPowers = new Pose(-relX, -relY, 0).scale(movementSpeed);
 
-        // Get heading that points from robot to target point. We want this to be the robot's heading
+        Pose reductionDistances = gunning ? GUNNING_REDUCTION_DISTANCES : FINE_REDUCTION_DISTANCES;
+        translationPowers.x /= reductionDistances.x;
+        translationPowers.y /= reductionDistances.y;
+
+        // Ensure we're moving x/y a little unless we're stopped
+        if (translationPowers.radius() < MIN_TRANSLATION_POWERS) {
+            translationPowers.scale(MIN_TRANSLATION_POWERS / translationPowers.radius());
+        }
+
+        // Heading always wants to stop at a point, so we'll treat this the same regardless if we're
+        // at a stop waypoint or a normal one
         double forwardAngle = target.minus(robotPose).atan();
         double backwardAngle = forwardAngle + Math.PI;
         double angleToForward = MathUtil.angleWrap(forwardAngle - robotPose.heading);
         double angleToBackward = MathUtil.angleWrap(backwardAngle - robotPose.heading);
         double autoAngle = Math.abs(angleToForward) < Math.abs(angleToBackward) ? forwardAngle : backwardAngle;
-
-        System.out.println(target instanceof HeadingControlledWaypoint);
-
         double desiredAngle = target instanceof HeadingControlledWaypoint ?
                 ((HeadingControlledWaypoint) target).targetHeading : autoAngle;
 
+        // We'll do a quadratic decay to zero to only correct for really big heading errors
         double angleToTarget = MathUtil.angleWrap(desiredAngle - robotPose.heading);
-        double turnPower = angleToTarget / reduceTurnSpeedThreshold;
-        return new MecanumPowers(xPower, yPower, turnPower);
+        translationPowers.heading = angleToTarget / reductionDistances.heading;
+        // We're going to square the turn power to reduce it when it is low
+        //translationPowers.heading = MathUtil.powRetainingSign(angleToTarget / REDUCTION_DISTANCES.heading, 1);
+        return new MecanumPowers(translationPowers);
     }
 }
