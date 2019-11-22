764c5937d3afd463bb38b4fc5569385e6493cd55
guberti <guberti@eastsideprep.org>
2019-11-22 11:42:28 -0800

    RevBulkData generator test

--- TeamCode/src/test/java/org/firstinspires/ftc/simulator/utils/MockRevBulkDataGeneratorTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/simulator/utils/MockRevBulkDataGeneratorTest.java
@@ -1,5 +1,28 @@
+package org.firstinspires.ftc.simulator.utils;
+
+import org.junit.jupiter.api.Test;
+import org.openftc.revextensions2.RevBulkData;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MockRevBulkDataGeneratorTest {
 
+    @Test
+    void testChangingMock() {
+        MockRevBulkDataGenerator gen = new MockRevBulkDataGenerator();
+        gen.encoderVals[0] = 5;
+
+        RevBulkData data = gen.mock();
+        assertEquals(5, data.getMotorCurrentPosition(0));
+        assertEquals(0, data.getMotorCurrentPosition(1));
+        assertEquals(0, data.getAnalogInputValue(0));
+        assertEquals(false, data.getDigitalInputState(0));
+
+        // Verify we can change the generator in real time, and that the mock responds accordingly
+        gen.encoderVals[1] = 120;
+        gen.digitalInputs[6] = true;
+
+        assertEquals(120, data.getMotorCurrentPosition(1));
+        assertEquals(true, data.getDigitalInputState(6));
+    }
 }
