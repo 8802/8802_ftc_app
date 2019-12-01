061ec18126f058dadf5878645871017ed51ca857
Gavin Uberti <guberti@eastsideprep.org>
2019-11-30 19:28:31 -0800

    Improve third block

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/SSAutoMovingFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/SSAutoMovingFoundation.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.autonomous.waypoints.StopWaypoint;
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.Subroutines;
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.Waypoint;
 import org.firstinspires.ftc.teamcode.common.elements.Alliance;
+import org.firstinspires.ftc.teamcode.common.elements.SkystoneState;
 import org.firstinspires.ftc.teamcode.common.math.Pose;
 import org.firstinspires.ftc.teamcode.robot.mecanum.MecanumPowers;
 import org.firstinspires.ftc.teamcode.robot.mecanum.MecanumUtil;
@@ -26,15 +27,12 @@ import static org.firstinspires.ftc.teamcode.robot.mecanum.SkystoneHardware.FIEL
 @Config
 public class SSAutoMovingFoundation extends PurePursuitAuto {
 
-    StopWaypoint DEPOSIT_LOCATION = new StopWaypoint(FIELD_RADIUS - 30, 36, 8,
-            Math.PI, 8, new ActionAndWait(1000, Subroutines.SMART_DROP_BLOCK));
-
     Waypoint GRAB_FOUNDATION_LOCATION = new StopWaypoint(FIELD_RADIUS - 10 - (34.5/2), 30,
             6, Math.PI * 0.5, 4, new FoundationGrabBackupPath());
 
     public static double PLUNGE_TARGET_Y = 22;
     public static double BACK_PLUNGE_TARGET_X = -FIELD_RADIUS + 13;
-    public static double FRONT_PLUNGE_TARGET_X = -FIELD_RADIUS + 40;
+    public static double FRONT_PLUNGE_TARGET_X = -FIELD_RADIUS + 38;
 
     @Override
     public Pose getBlueStartPosition() {
@@ -73,26 +71,41 @@ public class SSAutoMovingFoundation extends PurePursuitAuto {
                 new HeadingControlledWaypoint(FRONT_PLUNGE_TARGET_X + skystoneOffset, 36, 12, -Math.PI, Subroutines.GRAB_INTAKED_BLOCK),
                 // Now make our move to deposit
                 new HeadingControlledWaypoint(0, 36, 16, Math.PI),
-                new StopWaypoint(DEPOSIT_LOCATION.x, DEPOSIT_LOCATION.y, 8,
+                new StopWaypoint(20, 36, 8,
                         Math.PI, 8, new DepositUntilSuccessful(0))
         );
 
-        /* Now, we will "charge" along two straight lines until we encounter a block. Where those
-        lines are depends on what blocks we grabbed first, since if we grabbed SkystoneState.UPPER
-        the two skystones closest to the field wall haven't been touched.
+        /* What we do now depends on what blocks we grabbed first, since if we grabbed
+        SkystoneState.UPPER the two skystones closest to the field wall haven't been touched.
          */
 
-        scoreSkystones.addAll(Waypoint.collate(
-                new HeadingControlledWaypoint(36, 39, 6, Math.PI),
-                new HeadingControlledWaypoint(-20 + skystoneOffset, 39, 8, Math.PI, Subroutines.ENABLE_INTAKE),
-                new HeadingControlledWaypoint(-28 + skystoneOffset, 39, 8, Math.toRadians(225), Subroutines.CHECK_BLOCK_GRAB),
-                new HeadingControlledWaypoint(-44 + skystoneOffset, 20, 8, Math.toRadians(225), Subroutines.CHECK_BLOCK_GRAB),
-                new HeadingControlledWaypoint(-60, 20, 8, Math.PI, Subroutines.CHECK_BLOCK_GRAB),
+        if (SKYSTONE == SkystoneState.LOWER) {
+            scoreSkystones.addAll(Waypoint.collate(
+                    new HeadingControlledWaypoint(36, 39, 6, Math.PI),
+                    new HeadingControlledWaypoint(-20 + skystoneOffset, 39, 8, Math.PI, Subroutines.ENABLE_INTAKE),
+                    new HeadingControlledWaypoint(-28 + skystoneOffset, 39, 8, Math.toRadians(225), Subroutines.CHECK_BLOCK_GRAB),
+                    new HeadingControlledWaypoint(-44 + skystoneOffset, 20, 8, Math.toRadians(225), Subroutines.CHECK_BLOCK_GRAB),
+                    new HeadingControlledWaypoint(-65, 20, 8, Math.PI, Subroutines.CHECK_BLOCK_GRAB),
 
-                new Waypoint(-28 + skystoneOffset, 36, 8),
-                new HeadingControlledWaypoint(-20 + skystoneOffset, 39, 8, Math.PI),
+                    new Waypoint(-28 + skystoneOffset, 36, 8),
+                    new HeadingControlledWaypoint(-20 + skystoneOffset, 39, 8, Math.PI, Subroutines.GRAB_INTAKED_BLOCK)
+            ));
+        } else if (SKYSTONE == SkystoneState.MIDDLE || SKYSTONE == SkystoneState.UPPER) {
+            double shift = (SKYSTONE == SkystoneState.MIDDLE) ? SkystoneState.SKYSTONE_LENGTH : 0;
+            scoreSkystones.addAll(Waypoint.collate(
+                    new HeadingControlledWaypoint(-32, 36, 12, Math.PI, Subroutines.ENABLE_INTAKE),
+                    new HeadingControlledWaypoint(-42, 36, 6, Math.PI, Subroutines.ENABLE_INTAKE),
+
+                    new HeadingControlledWaypoint(-50, 36, 6, Math.toRadians(225), Subroutines.CHECK_BLOCK_GRAB),
+                    new StopWaypoint(-58, 28, 4, Math.toRadians(225),
+                            3, new JoltsUntilBlockGrab(joltDirection)),
+                    new HeadingControlledWaypoint(-54, 36, 12, -Math.PI, Subroutines.GRAB_INTAKED_BLOCK)
+            ));
+        }
+
+        scoreSkystones.addAll(Waypoint.collate(
                 new HeadingControlledWaypoint(0, 39, 8, Math.PI, Subroutines.LIFT_LEVEL_ONE),
-                new StopWaypoint(DEPOSIT_LOCATION.x, DEPOSIT_LOCATION.y, 8,
+                new StopWaypoint(40, 36, 8,
                         Math.PI, 8, new DepositUntilSuccessful(1))
         ));
 
