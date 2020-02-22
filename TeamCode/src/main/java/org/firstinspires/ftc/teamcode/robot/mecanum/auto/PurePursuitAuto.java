c7174d296db89e61ed73feccf901b7124df59fd1
Gavin Uberti <guberti@eastsideprep.org>
2020-02-21 17:12:12 -0800

    Auto doesn't break lift and displays skystone position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/PurePursuitAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/PurePursuitAuto.java
@@ -7,11 +7,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.autonomous.PurePursuitPath;
+import org.firstinspires.ftc.teamcode.autonomous.waypoints.StopWaypoint;
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.Waypoint;
 import org.firstinspires.ftc.teamcode.common.SimulatableMecanumOpMode;
 import org.firstinspires.ftc.teamcode.common.elements.Alliance;
@@ -27,6 +29,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvWebcam;
 import org.openftc.revextensions2.RevBulkData;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
@@ -38,6 +41,7 @@ public abstract class PurePursuitAuto extends SimulatableMecanumOpMode {
 
     SkystoneHardware robot;
     PurePursuitPath followPath;
+    private Telemetry.Item skystoneSeen;
 
     // Robot state
     public static SkystoneState SKYSTONE = SkystoneState.UPPER;
@@ -45,6 +49,7 @@ public abstract class PurePursuitAuto extends SimulatableMecanumOpMode {
 
     public abstract Pose getBlueStartPosition();
     public abstract List<Waypoint> getPurePursuitWaypoints();
+
 
     @Override
     public void init() {
@@ -58,14 +63,17 @@ public abstract class PurePursuitAuto extends SimulatableMecanumOpMode {
         robot.pidLift.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.pidLift.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         startPhoneCamDetector(ALLIANCE);
-        telemetry.clearAll();
-        robot.pidLift.left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.pidLift.right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.pidLift.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.pidLift.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Display skystone on driver station
+        skystoneSeen = telemetry.addLine().addData("Skystone", "LOADING");
+
     }
 
     @Override
     public void init_loop() {
-        //telemetry.log().add(getSkystoneState().toString());
+        skystoneSeen.setValue(getSkystoneState().toString());
         telemetry.update();
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/TestPurePursuitAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/mecanum/auto/TestPurePursuitAuto.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.robot.mecanum.auto;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.autonomous.PurePursuitPath;
+import org.firstinspires.ftc.teamcode.autonomous.waypoints.StopWaypoint;
 import org.firstinspires.ftc.teamcode.autonomous.waypoints.Waypoint;
 import org.firstinspires.ftc.teamcode.common.SimulatableMecanumOpMode;
 import org.firstinspires.ftc.teamcode.common.elements.Alliance;
@@ -12,62 +14,40 @@ import org.firstinspires.ftc.teamcode.common.elements.SkystoneState;
 import org.firstinspires.ftc.teamcode.common.math.Pose;
 import org.firstinspires.ftc.teamcode.robot.mecanum.MecanumUtil;
 import org.firstinspires.ftc.teamcode.robot.mecanum.SkystoneHardware;
-import org.firstinspires.ftc.teamcode.robot.mecanum.auto.vision.ImprovedSkystoneDetector;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.revextensions2.RevBulkData;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.firstinspires.ftc.teamcode.robot.mecanum.SkystoneHardware.FIELD_RADIUS;
 
-@Config
-public abstract class PurePursuitAutoRed extends SimulatableMecanumOpMode {
-    Pose DEFAULT_START_POSITION = new Pose(-FIELD_RADIUS + 22.75 + 9, -FIELD_RADIUS + 9, 1 * Math.PI / 2);
+@Autonomous(name="Test Pure Pursuit", group="Testing")
+public abstract class TestPurePursuitAuto extends SimulatableMecanumOpMode {
+    Pose DEFAULT_START_POSITION = new Pose(-FIELD_RADIUS + 22.75 + 9, FIELD_RADIUS - 9, 3 * Math.PI / 2);
 
     SkystoneHardware robot;
     PurePursuitPath followPath;
-    private Telemetry.Item skystoneSeen;
 
     // Robot state
-    public static SkystoneState SKYSTONE = SkystoneState.UPPER;
-    public static Alliance ALLIANCE = Alliance.RED;
 
-    public abstract Pose getBlueStartPosition();
     public abstract List<Waypoint> getPurePursuitWaypoints();
 
     @Override
     public void init() {
-        Pose start = getBlueStartPosition().clone();
+        Pose start = DEFAULT_START_POSITION.clone();
         this.robot = this.getRobot(start);
-
-        // During this we're also going to init our lift again
-        robot.pidLift.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.pidLift.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        startPhoneCamDetector(ALLIANCE);
-        robot.pidLift.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.pidLift.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        // Display skystone on driver station
-        skystoneSeen = telemetry.addLine().addData("Skystone", "LOADING");
-    }
-
-    @Override
-    public void init_loop() {
-        skystoneSeen.setValue(getSkystoneState().toString());
-        telemetry.update();
     }
 
     @Override
     public void start() {
         telemetry.clearAll();
         robot.initBulkReadTelemetry();
-        SKYSTONE = getSkystoneState();
-        stopPhoneCamDetector();
-        followPath = new PurePursuitPath(robot, getPurePursuitWaypoints());
-        // We design all paths for blue side, and then flip them for red
-        followPath.reverse();
+        LinkedList<Waypoint> waypoints = Waypoint.collate(
+                new Waypoint(DEFAULT_START_POSITION, 4),
+                new StopWaypoint(DEFAULT_START_POSITION.x, -38.5, 12, 1.5 * Math.PI, 0)
+        );
+
+        followPath = new PurePursuitPath(robot, waypoints);
     }
 
     @Override
